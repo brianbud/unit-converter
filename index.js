a16a6afb39d155abb593ee3676e80d37c2435b93
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-11 18:27:51 -0500

    display input

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const convertEl = document.getElementById('convert-btn');
+const inputEl = document.getElementById('input-el');
+let inputNumEl = document.getElementsByClassName('input-num');
+console.log(inputNumEl.length);
+
+convertEl.addEventListener('click', convert);
+
+function convert() {
+  for (let i = 0; i <= inputNumEl.length; i++) {
+    inputNumEl[i].textContent = inputEl.value;
+  }
+}
