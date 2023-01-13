f1e9a1ec5199d64f352f0b8d42603bec717ffcd4
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-13 11:53:23 -0500

    up to 2 decimal points

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ button.addEventListener('click', setCalc);
 function setCalc() {
   let input = inputEl.value;
   metersFeet.innerHTML = `
-      ${input} meters = ${input * 3.281} feet | 
-      ${input} feet = ${input * 0.3048} meters
+      ${input} meters = ${(input * 3.281).toFixed(2)} feet | 
+      ${input} feet = ${(input * 0.3048).toFixed(2)} meters
     `;
 
   litersGallons.innerHTML = `
-      ${input} liters = ${input * 0.264172} gallons |
-      ${input} gallons = ${input * 3.78541} liters
+      ${input} liters = ${(input * 0.264172).toFixed(2)} gallons |
+      ${input} gallons = ${(input * 3.78541).toFixed(2)} liters
     `;
 
   kilosPounds.innerHTML = `
-      ${input} kilos = ${input * 2.20462} pounds | 
-      ${input} pounds = ${input / 2.20462} kilos
+      ${input} kilos = ${(input * 2.20462).toFixed(2)} pounds | 
+      ${input} pounds = ${(input / 2.20462).toFixed(2)} kilos
     `;
 }
